bc8ce2d8a3bca179add4430558013096d7a64d74
Sean Houde <seanhoude@gmail.com>
2019-02-14 13:25:50 -0500

    add List and Task models, update admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from core.models import User, List, Task
 
 # Register your models here.
+
+class ListAdmin(admin.ModelAdmin):
+    model = List
+    list_display = (
+        "title",
+        "user",
+        "date",
+        "completed",
+    )
+
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+    list_display = (
+        "name",
+        "time_allocated",
+        "list",
+        "category",
+        "completed",
+    )
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Task, TaskAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TimeStamp(models.Model):
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+
+class List(TimeStamp):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('title', 'date', 'user', )
+
+    def __str__(self):
+        return self.title
+
+class Task(TimeStamp):
+    name = models.CharField(max_length=250)
+    time_allocated = models.DurationField()
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name="tasks")
+    category = models.CharField(max_length=20)
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('list', 'name', )
+
+    def __str__(self):
+        return self.name
